3f180d890c9add1f67b9386cc183254ac1479d5e
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2017-12-31 17:30:26 +0200

    Add css, less, style loaders to showcase webpack

--- showcase/webpack.config.js
+++ showcase/webpack.config.js
@@ -33,6 +33,17 @@ module.exports = {
 					path.resolve(__dirname, './webpack.config.js'),
 				],
 			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+					'less-loader'
+				],
+				include: [
+					path.resolve(__dirname, '../src'),
+				],
+			},
 		],
 	},
 	plugins: [
