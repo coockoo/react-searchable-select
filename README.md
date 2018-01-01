64fa676a0431267f9d544503a678abba6dd38837
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2018-01-01 20:44:15 +0200

    Add moment-timezone to showcase. Add initial README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-searchable-select
+
+## Showcase
+
+This project was developed in *node v9.2.1* environment. To start a showcase simply
+
+```bash
+npm install
+npm start
+```
+
+Open in your favourite web browser [http://localhost:8080](http://localhost:8080)
+
+## Todo
+
+- Add build process for `src` into `dist` (without showcase)
+- Check for performance issues and avoiding possible re-renders with `why-did-you-update`
--- showcase/app.js
+++ showcase/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import queryTimezones from './query-timezones';
+import moment from 'moment-timezone';
 
 import SearchableSelect from '../src';
 
@@ -12,6 +13,7 @@ class App extends Component {
 			options: [],
 			availableOptions: [],
 			limit: 10,
+			currentMoment: moment(),
 		};
 		this.state.availableOptions = this.state.options.slice(0, this.state.limit)
 	}
@@ -22,7 +24,14 @@ class App extends Component {
 				options,
 				availableOptions: options.slice(0, this.state.limit)
 			})
+			this.interval = setInterval(() => {
+				this.setState({ currentMoment: moment() })
+			}, 1000)
 		})
+	}
+	componentWillUnmount () {
+		this.interval && clearInterval(this.interval)
+		this.interval = null
 	}
 	handleSelectChange (value) {
 		this.setState({ value })
@@ -47,8 +56,14 @@ class App extends Component {
 					/>
 				</div>
 				<p>This is my text after the selector. Just random content.</p>
-				<p>Current value is:</p>
+				<h3>Current value is:</h3>
 				<pre>{JSON.stringify(this.state.value)}</pre>
+				{this.state.value && this.state.value.value ? (
+					<div>
+						<h3>Current time in selected timezone is:</h3>
+						<p>{this.state.currentMoment.tz(this.state.value.value).format('YYYY-MM-DD HH:mm:ss')}</p>
+					</div>
+				) : null}
 			</div>
 		);
 	};
