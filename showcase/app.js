fcd722c239070e057bda9f05eaec39ad12462b65
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2018-01-01 18:46:41 +0200

    Add value/search change for searchable-select component on clicks/edits

--- showcase/app.js
+++ showcase/app.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 
 import SearchableSelect from '../src'
 
-function App () {
-	return (
-		<SearchableSelect
-			value={'Europe/Kyiv'}
-			options={[
-				{ label: 'Europe/Kyiv', value: 'Europe/Kyiv' },
-				{ label: 'Europe/Athens', value: 'Europe/Athens' },
-			]}
-		/>
-	)
+class App extends Component {
+	constructor () {
+		super()
+		this.state = {
+			value: null
+		};
+	}
+	handleSelectChange (value) {
+		this.setState({ value })
+	}
+	render () {
+		return (
+			<SearchableSelect
+				value={this.state.value}
+				options={[
+					{ label: 'Europe/Kyiv', value: 'Europe/Kyiv' },
+					{ label: 'Europe/Athens', value: 'Europe/Athens' },
+				]}
+				onChange={(value) => this.handleSelectChange(value)}
+			/>
+		);
+	};
 }
 
 export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import cn from 'classnames';
 
 import s from './styles.less';
 
@@ -6,29 +7,54 @@ class SearchableSelect extends Component {
 	constructor () {
 		super();
 		this.state = {
-			search: ''
+			search: '',
+			isEditing: false,
 		};
 	}
-	componentDidMount () {
+	handleSearchChange (e) {
+		const newSearch = e.target.value;
+		this.setState({ search: newSearch });
+		this.props.onSearchChange(newSearch);
 	}
-	onSearchChange (e) {
-		const newSearch = e.target.value
-		this.setState({ search: newSearch })
-		this.props.onSearchChange(newSearch)
+	handleOptionClick (option) {
+		this.props.onChange(option.value);
+		this.setState({
+			search: option.value,
+			isEditing: false,
+		});
+	}
+	handleValueClick () {
+		this.setState({
+			search: this.props.value || '',
+			isEditing: true,
+		}, () => {
+			this.searchInput.focus();
+		});
 	}
 	render () {
 		return (
 			<div className={s.container}>
+				<div
+					className={cn(s.value, { [s.hidden]: this.state.isEditing })}
+					onClick={() => this.handleValueClick()}
+				>
+					<span>{this.props.value}</span>
+				</div>
 				<div className={s.input}>
 					<input
+						ref={(input) => { this.searchInput = input; }}
 						type="text"
 						value={this.state.search}
-						onChange={(e) => this.onSearchChange(e)}
+						onChange={(e) => this.handleSearchChange(e)}
+						className={cn({ [s.hidden]: !this.state.isEditing })}
 					/>
 				</div>
 				<ul className={s.options}>
 					{this.props.options.map(option => (
-						<li key={option.value}>
+						<li
+							key={option.value}
+							onClick={() => this.handleOptionClick(option)}
+						>
 							{option.label}
 						</li>
 					))}
